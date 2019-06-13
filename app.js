28cf005d0af1976f7e8537f4092828ae67f9993f
David Hambraeus <ddhambraeus@gmail.com>
2019-06-13 15:03:03 +0200

    Added stuff to facilitate hosting the pictures ourselves

--- app.js
+++ app.js
@@ -1,16 +1,53 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+var express = require('express');
+var fs = require('fs');
+var app = express();
 
-const app = express();
 
-app.use(express.static(__dirname));
-app.use(bodyParser.json());
+function js(){
+    //var scriptPath = __dirname + "/bassida.js"
+    //var scriptPath = __dirname + "/frontend_custom_everything/index.html"
+    var scriptPath = __dirname + "/frontend/index.html"
+    var options = {encoding:'utf-8', flag:'r'};
 
-app.get('/image/:id', (req, res) => {
-    res.json('Fetching image ' + req.params.id);
+    var buffer = fs.readFileSync(scriptPath, options);
+    return buffer;
+};
+
+// Generates urls on the form (urlstart + category + filename) where category is the name of the folders in bildfolder and filename is the names of the files in said folder. 
+function generatePicUrls(){
+    var bildfolder = __dirname + '/images/'
+
+    function addBildUrls (category) {
+        var urlstart = 'http://localhost:3000/images/';
+
+        var suburls = [];
+        fs.readdirSync(bildfolder + category).forEach(function(file) {
+            if (file != "thumbnails") {
+                suburls.push(urlstart + category + '/' + file);
+            }
+        });
+        return suburls
+    }
+
+    var urls = {};
+
+    fs.readdirSync(bildfolder).forEach(function(category) {
+        urls[category] = addBildUrls(category);
+    });
+
+    return JSON.stringify(urls);
+}
+
+
+app.get('/', function(req, res){
+	res.send('<script> var urls = ' + generatePicUrls() + ";</script>" + js());
 });
 
 
-app.listen(3000, () => {
-    console.log('Express listening on port 3000');
-});
+var images = require('./images.js');
+app.use('/images', images);
+
+var frontend = require('./frontend.js');
+app.use('/frontend', frontend);
+
+app.listen(3000);
