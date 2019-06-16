db9ef3a785e2126a0f17b3448cd12e79117b424b
Eric Carlsson <ericca@student.chalmers.com>
2019-06-16 18:14:13 +0200

    Added get /album/id

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
-var fs = require('fs');
-const { check, validationResult } = require('express-validator/check');
+const fs = require('fs');
+const path = require('path');
+const { param, check, validationResult } = require('express-validator/check');
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -8,13 +9,16 @@ const app = express();
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 
+const filespath = __dirname + '/files/';
+
+const extensions = ['.jpg', '.jpeg', '.jpe', '.jif', '.jfif', '.jfi', '.gif', '.png',
+'.bmp', '.dib'];
+
 
 // Returns JSON containing list of all albums in ascending order
-app.get('/album', (req, res) => {
-    fs.readdir(__dirname + '/files', { withFileTypes: true }, (err, files) => {
-        if (err) {
-            next(err);
-        }
+app.get('/album', (req, res, next) => {
+    fs.readdir(filespath, { withFileTypes: true }, (err, files) => {
+        if (err) next(err);
         else {
             var directories = files.filter((item) => {
                 return item.isDirectory();
@@ -28,8 +32,25 @@ app.get('/album', (req, res) => {
 // Returns JSON containing album id, name, cover image, photographer(s) and list of images contained in album in ascending order
 app.get('/album/:id', [
     param('id').isString().escape()
-], (req, res) => {
-    
+], (req, res, next) => {
+    var response = {};
+    fs.readdir(filespath + req.params.id, { withFileTypes: true }, (err, files) => {
+        if (err) next(err);
+        else {
+            var images = files.filter((item) => {
+                return extensions.includes(path.extname(item.name));
+            });
+            response.images = images;
+        }
+    });
+    fs.readFile(filespath + req.params.id + '/meta.json', (err, data) => {
+        if (err) next(err);
+        else {
+            var metadata = JSON.parse(data);
+            console.log(metadata);
+        }
+    });
+    return res.json(response);
 });
 
 // Returns JSON containing image path and thumbnail path
