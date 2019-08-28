1b3ce9c8587b5d81be75ffcc1a031f937758656f
Eric Carlsson <ericca@student.chalmers.com>
2019-08-28 22:09:03 +0200

    change footer order

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,15 +5,15 @@ export default function Footer() {
   return (
     <footer>
       <div>
-        Om du vill använda bilder eller vill få bilder borttagna, kontakta oss via <a href="mailto:foton@ftek.se">foton@ftek.se</a> eller via vår <a href="https://www.facebook.com/FotonChalmers/" target="_blank" rel="noopener noreferrer">Facebooksida</a>.
-      </div>
-      <div>
         <a href="https://www.facebook.com/FotonChalmers/" target="_blank" rel="noopener noreferrer" className="logo logo-facebook">
           <img src={facebook} alt="Besök oss på Facebook" />
         </a>
       </div>
       <div>
         <a href="https://github.com/ECarlsson/foton" target="_blank" rel="noopener noreferrer">GitHub</a> | <a href="https://github.com/ECarlsson/foton/blob/master/readme.md#api" target="_blank" rel="noopener noreferrer">API</a>
+      </div>
+      <div>
+        Om du vill använda bilder eller vill få bilder borttagna, kontakta oss via <a href="mailto:foton@ftek.se">foton@ftek.se</a> eller via vår <a href="https://www.facebook.com/FotonChalmers/" target="_blank" rel="noopener noreferrer">Facebooksida</a>.
       </div>
       <div>&copy; {(new Date()).getFullYear()} Fysikteknologsektionen</div>
     </footer>
