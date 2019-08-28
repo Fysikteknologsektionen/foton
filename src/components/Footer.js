7b5667599f126f170a418bf1625af5749718feb6
Eric Carlsson <ericca@student.chalmers.com>
2019-08-28 21:25:40 +0200

    fix facebook logo size

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,12 +5,12 @@ export default function Footer() {
   return (
     <footer>
       <div>
+        Om du vill använda bilder eller vill få bilder borttagna, kontakta oss via <a href="mailto:foton@ftek.se">mailto:foton@ftek.se</a> eller via vår <a href="https://www.facebook.com/FotonChalmers/" target="_blank" rel="noopener noreferrer">facebooksida</a>.
+      </div>
+      <div>
         <a href="https://www.facebook.com/FotonChalmers/" target="_blank" rel="noopener noreferrer" className="logo logo-facebook">
           <img src={facebook} alt="Besök oss på Facebook" />
         </a>
-      </div>
-      <div>
-        Om du vill använda bilder eller vill få bilder borttagna, kontakta oss via <a href="mailto:foton@ftek.se">mailto:foton@ftek.se</a> eller via vår <a href="https://www.facebook.com/FotonChalmers/" target="_blank" rel="noopener noreferrer">facebooksida</a>.
       </div>
       <div>
         <a href="https://github.com/ECarlsson/foton" target="_blank" rel="noopener noreferrer">GitHub</a> | <a href="https://github.com/ECarlsson/foton/blob/master/readme.md#api" target="_blank" rel="noopener noreferrer">API</a>
