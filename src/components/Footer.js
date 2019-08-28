481d71951279ad2f64050556e8b136cb069e571f
Eric Carlsson <ericca@student.chalmers.com>
2019-08-28 21:14:41 +0200

    Add copyright info to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import facebook from './facebook.eps';
 
 export default function Footer() {
   return (
     <footer>
+      <div>
+        <a href="https://ftek.se/" target="_blank" rel="noopener noreferrer" className="logo logo-facebook">
+          <img src={facebook} alt="Besök oss på Facebook" />
+        </a>
+      </div>
+      <div>
+        Om du vill använda bilder eller vill få bilder borttagna, kontakta oss via <a href="mailto:foton@ftek.se">mailto:foton@ftek.se</a> eller via vår <a href="https://www.facebook.com/FotonChalmers/" target="_blank" rel="noopener noreferrer">facebooksida</a>.
+      </div>
       <div>
         <a href="https://github.com/ECarlsson/foton" target="_blank" rel="noopener noreferrer">GitHub</a> | <a href="https://github.com/ECarlsson/foton/blob/master/readme.md#api" target="_blank" rel="noopener noreferrer">API</a>
       </div>
