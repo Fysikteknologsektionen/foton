54aedfd6c5ea3b39861fe21e38cc21d8390f5d45
Eric Carlsson <ericca@student.chalmers.com>
2019-09-02 16:24:17 +0200

    Fix for filenames containing spaces

--- shellscripts/createThumbnails.sh
+++ shellscripts/createThumbnails.sh
@@ -17,10 +17,16 @@ do
     	mkdir ${pathToAlbum}/thumbnails
     fi
 
-    # Replace non-url-friendly characters with friendly equivalents 
-	rename -v -n 's/ÅÄÖåäö /AAOaao-/g' $pathToAlbum
+    IFS=$'\n'
+    # Create a list of all pictures in the folder
+    files=$(ls ${pathToAlbum} | grep -vE "thumbnails|meta.json")
+    for file in $files
+    do
+        # Replace non-url-friendly characters with friendly equivalents 
+        mv "$pathToAlbum"/"$file" $(echo "$pathToAlbum"/"$file" | sed 'y/ÅÄÖåäö /AAOaao-/')
+    done
 
     # Use mogrify to bulk convert images
-    mogrify -path ${pathToAlbum}/thumbnails -filter Triangle -define filter:support=2 -thumbnail 420 -unsharp 0.25x0.25+8+0.065 -dither None -posterize 136 -quality 82 -define jpeg:fancy-upsampling=off -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 -define png:exclude-chunk=all -interlace none -colorspace sRGB -strip ${pathToAlbum}/*
+    mogrify -path "${pathToAlbum}/thumbnails" -filter Triangle -define filter:support=2 -thumbnail 420 -unsharp 0.25x0.25+8+0.065 -dither None -posterize 136 -quality 82 -define jpeg:fancy-upsampling=off -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 -define png:exclude-chunk=all -interlace none -colorspace sRGB -strip "${pathToAlbum}/*"
 done
 
